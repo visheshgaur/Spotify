4c02ab4a5c997d21fe881716fdfef9a7fb3786f2
vishesh <gaurvishesh999@gmail.com>
2026-04-29 23:10:29 +0530

    Setup

--- server.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config()
-const app=require("./src/app")
-const connectDb=require("./src/db/db")
-connectDb()
-app.listen(3000,()=>{
-        console.log("Running on port 3000")
-})
--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const router=express.Router();
-const authController=require("../controllers/auth.controller")
-router.post("/register",authController.registerUser)
-router.post("/login",authController.loginUser)
-router.post("/logout",authController.logOutUser)
-
-module.exports=router;
